bd247ce043855ea702259ea7f9eecf1461241b5e
QV Nguyen <qv.nguyen@gmail.com>
2022-05-10 00:21:56 +0200

    init Cart Controller

--- server/src/controllers/CartController.js
+++ server/src/controllers/CartController.js
@@ -2,11 +2,44 @@ const { User, Book, Cart, CartItem } = require('../database/models')
 const { errorHandler, errorType } = require('./ErrorHandler')
 
 module.exports = {
+  async parseCart (req, res, next) {
+    try {
+      const idCart = parseInt(req.params.id_cart)
+
+      if (isNaN(idCart)) {
+        throw new Error('id_cart must be a number')
+      }
+
+      req.id_cart = idCart
+      
+      next()
+    } catch (err) {
+      errorHandler(res, err, 'Cannot parse cart id')
+    }
+  },
   async create (req, res) {
     try {
-      
-    } catch (err) {
+      const [cart] = Cart.findOrCreate({
+        where: {
+          id_user: req.id_user,
+          status: 'pending'
+        }
+      })
 
+      res.status(201).send(cart)
+    } catch (err) {
+      errorHandler(res, err, 'Cannot create new cart')
+    }
+  },
+  async read (req, res) {
+    try {
+      const carts = Cart.findAll({
+        where: {
+          id_user: req.id_user
+        }
+      })
+    } catch (err) {
+      errorHandler(res, err, 'Cannot fetch carts')
     }
   }
 }
