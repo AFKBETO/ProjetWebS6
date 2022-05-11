bfe4502ad2c769fdc3307ba310a53a1a2ad761c3
QV Nguyen <qv.nguyen@gmail.com>
2022-05-11 13:08:11 +0200

    indentation fixes

--- server/src/database/models/index.js
+++ server/src/database/models/index.js
@@ -5,53 +5,53 @@ const config = require('../config/config')
 const db = {}
 
 const sequelize = new Sequelize(
-    config.db.database,
-    config.db.user,
-    config.db.password,
-    config.db.options
+  config.db.database,
+  config.db.user,
+  config.db.password,
+  config.db.options
 ) 
 
 fs
-    .readdirSync(__dirname)
-    .filter((file) => 
-        file != 'index.js'
-    )
-    .forEach((file) => {
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
-        db[model.name] = model
-    })
+  .readdirSync(__dirname)
+  .filter((file) => 
+    file != 'index.js'
+  )
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    db[model.name] = model
+  })
 
 db['User'].hasMany(db['Cart'], {
-    foreignKey: 'id_user',
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
+  foreignKey: 'id_user',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 })
 db['Cart'].belongsTo(db['User'], {
-    foreignKey: 'id_user',
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
+  foreignKey: 'id_user',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 })
 
 db['Cart'].hasMany(db['CartItem'], {
-    foreignKey: 'id_cart',
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
+  foreignKey: 'id_cart',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 })
 db['CartItem'].belongsTo(db['Cart'], {
-    foreignKey: 'id_user',
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
+  foreignKey: 'id_user',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 })
 
 db['Book'].hasMany(db['CartItem'], {
-    foreignKey: 'id_book',
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
+  foreignKey: 'id_book',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 })
 db['CartItem'].belongsTo(db['Book'], {
-    foreignKey: 'id_book',
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
+  foreignKey: 'id_book',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 })
 
 db.sequelize = sequelize
