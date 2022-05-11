d68d431db5d56be247b7b60f40aeb3128be7de8b
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-12 00:29:04 +0200

    tweak init book

--- server/tests/devinit/book.init.js
+++ server/tests/devinit/book.init.js
@@ -2,26 +2,31 @@ const { Book } = require('../../src/database/models')
 
 const books = [
   {
+    id_book: 1,
     name_book: '1984',
     url: 'https://images-na.ssl-images-amazon.com/images/I/81StSOpmkjL.jpg',
     quantity_book: 4
   },
   {
+    id_book: 2,
     name_book: 'Das Kapital',
     url: 'https://images-na.ssl-images-amazon.com/images/I/71yUG55s-CL.jpg',
     quantity_book: 2
   },
   {
+    id_book: 3,
     name_book: 'Happycratie',
     url: 'https://sp-ao.shortpixel.ai/client/to_webp,q_lossless,ret_img/https://catalog.2seasagency.com/wp-content/uploads/2018/04/HAPPYCRACY-e1534861029896-200x267.jpg',
     quantity_book: 6
   },
   {
+    id_book: 4,
     name_book: 'The Wealth of Nations',
     url: 'https://m.media-amazon.com/images/I/51zz0eUJNHL.jpg',
     quantity_book: 9
   },
   {
+    id_book: 5,
     name_book: 'Harry Potter',
     url: 'http://prodimage.images-bn.com/pimages/9780545139700_p0_v5_s1200x630.jpg',
     quantity_book: 7
