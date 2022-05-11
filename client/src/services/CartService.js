6aab026366d1e56e5132cc3f8523269891863815
QV Nguyen <qv.nguyen@gmail.com>
2022-05-11 13:09:37 +0200

    add + implement CartService

--- client/src/services/CartService.js
+++ client/src/services/CartService.js
@@ -1,5 +1,16 @@
 import { Api, setBearer } from '@/services/Api'
 
-export function fetchCart() {
-  
+export function fetchCarts () {
+  setBearer()
+  return Api().get('carts')
 }
+
+export function getCurrentCart () {
+  setBearer()
+  return Api().put('carts')
+}
+
+/*
+export function addToCart (idBook) {
+  setBearer()
+} */
--- client/src/views/BookView.vue
+++ client/src/views/BookView.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="books">
-    <div class="h1">Book Catalogue</div>
+    <div class="d-flex justify-content-around">
+      <div class="h1 text-left me-auto">Book Catalogue</div>
+      <div class="p-1">
+        <div class="input-group">
+          <div class="p-0">
+            <input type="search" class="form-control" placeholder="Search" v-model="searchBox" />
+          </div>
+          <span class="btn btn-secondary input-group-text">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+          </span>
+        </div>
+      </div>
+    </div>
     <div class="container-sm">
       <div class="row row-cols-4 g-2">
         <BookComponent
           class="col border p-0"
-          v-for="book in books"
-          :key="book.id_book"
+          v-for="(book, index) in books"
+          :key="index"
           :name_book="book.name_book"
           :url="book.url"
           :quantity_book="book.quantity_book"
-          :borrowedQty="book.borrowedQty" />
+          :borrowedQty="book.borrowedQty"
+          :quantity_cart="book.quantity_borrowed"
+          v-show="book.name_book.includes(searchBox)" />
       </div>
     </div>
   </div>
@@ -18,6 +34,7 @@
 
 <script>
 import { fetchBooks } from '@/services/BookService.js'
+import { getCurrentCart } from '@/services/CartService.js'
 import BookComponent from '@/components/BookComponent.vue'
 
 export default {
@@ -26,12 +43,45 @@ export default {
   },
   data () {
     return {
-      books: []
+      books: {},
+      cart: {},
+      searchBox: '',
+      loaded: false
     }
   },
-  mounted: async function () {
-    const books = await fetchBooks()
-    this.books = books.data
+  watch: {
+    loaded (newValue) {
+      if (newValue) {
+        this.$forceUpdate()
+      }
+    }
+  },
+  beforeMount: async function () {
+    let response = await fetchBooks()
+    const books = response.data
+    if (books) {
+      for (const book of books) {
+        this.books[book.id_book] = book
+        this.books[book.id_book]['quantity_borrowed'] = 0
+      }
+    }
+    response = await getCurrentCart()
+    const cart = response.data[0]
+    if (cart) {
+      const cartItems = cart.CartItems
+      if (cartItems) {
+        for (const cartItem of cartItems) {
+          this.cart[cartItem.id_book] = cartItem
+          this.books[cartItem.id_book].quantity_borrowed = cartItem.quantity_cart
+        }
+      }
+    }
+    this.loaded = true
+  },
+  computed: {
+    searchFilter () {
+      return this.searchBox
+    }
   }
 }
 </script>
