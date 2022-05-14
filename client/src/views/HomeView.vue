61e296d62c345384900b91dc5264a5c764227314
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-14 22:13:12 +0200

    fix offcanvas button

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -29,7 +29,7 @@
           </div>
         </div>
       </div>
-      <button class="btn btn-secondary fixed-top" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbaroffcanvas" aria-controls="navbaroffcanvas">
+      <button class="btn btn-secondary btn-offcanvas" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbaroffcanvas" aria-controls="navbaroffcanvas">
         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="43" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
         </svg>
@@ -91,6 +91,11 @@ export default {
 }
 .block-offcanvas {
   visibility: hidden;
+  position: relative;
+}
+
+.btn-offcanvas {
+  position: absolute;
 }
 .fill{
   height: 100%;
@@ -104,6 +109,11 @@ export default {
   }
   .block-offcanvas {
     visibility: visible;
+  }
+
+  .btn-offcanvas {
+
+    top: -57px;
   }
 }
 </style>
