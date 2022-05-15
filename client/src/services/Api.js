5e7c4025f7fe1c500910c9e03187ffe04b4d7bb8
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-15 23:19:23 +0200

    cleaning up

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,12 +3,7 @@ import axios from 'axios'
 export function Api () {
   return axios.create({
     baseURL: `http://localhost:3001/api/`,
-    withCredentials: true,
-    headers: {
-      'Access-Control-Allow-Origin': 'http://localhost:3001',
-      'Access-Control-Allow-Credentials': true,
-      'Content-Type': 'application/json'
-    }
+    withCredentials: true
   })
 }
 
--- server/src/controllers/VerifyController.js
+++ server/src/controllers/VerifyController.js
@@ -5,9 +5,8 @@ const { errorHandler, errorType } = require('./ErrorHandler')
 module.exports = {
   jwtVerifyUser (req, res, next) {
     try {
-    console.log(req.cookies.token)
-    const token = req.cookies.token
-    if (token) {
+      const token = req.cookies.token
+      if (token) {
         const decoded = jwt.verify(token, config.authentication.jwtSecret)
         req.id_user = decoded.id_user
         req.profile = decoded.profile
